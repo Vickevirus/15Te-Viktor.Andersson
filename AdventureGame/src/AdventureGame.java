83f21c83fd502ffc527503ac660fa0583c80f53f
Vickevirus <viktor.andersson@elev.it-gymnasiet.se>
2017-02-08 12:41:45 +0100

    Äventyruppgift
    Påbörjar uppgiften

---
+++ AdventureGame/src/AdventureGame.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class AdventureGame {
+
+	static float hälsa = 100;
+	static int pengar = 10;
+	static String förnamn = "";
+	static String efternamn = "";
+	static String spelarnamn = "";
+	static int ålder;
+	static int sträcka = 0;
+	static int posX = 0;
+	static int posY = 0;
+	
+	public static void main(String[] args) {
+		
+		// presentera spelet
+		System.out.println("Ett Äventyrsspel!");
+		System.out.println("LYCKA TILL!");
+		//Läs in spelarens namn och karaktärsnamn
+		
+		//Börja spelet
+		
+		String kommando = "";
+		System.out.print("Vad ska vi göra? ");
+		Scanner in = new Scanner(System.in);
+		kommando = in.nextLine();
+			
+		// Håll på så långe det magiska ordet INTE är matat
+		while ( !(kommando.equals("Exit!!")))
+		{
+			if(kommando.equalsIgnoreCase("N")){
+				System.out.println("Norr!");
+				posY +=1;
+			}
+			
+			if(kommando.equalsIgnoreCase("S")){
+				System.out.println("Söder!");
+				posY -=1;
+			}
+			
+			if(kommando.equalsIgnoreCase("Ö")){
+				System.out.println("Öster!");
+				posX +=1;
+				
+			}
+			
+			if(kommando.equalsIgnoreCase("V")){
+				System.out.println("Väster");
+				posX -=1;
+			}
+			hälsa -=0.1;
+			System .out.println("Du är på("+posX+","+posY+") och har hälsan:"+hälsa);
+			
+			
+			System.out.print("Vad ska vi göra? ");
+			kommando = in.nextLine();
+							
+		}
+			
+		System.out.println("Hejdååååå");
+
+	
+
+	}
+
+}
